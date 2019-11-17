8d97ccd63a5b0e06c2e42aa96e01a0fc89eee6b4
Gregory Travis <greg.m.travis@gmail.com>
2019-11-17 13:48:21 -0500

    oop

---
+++ src/Sequence.hs
@@ -0,0 +1,54 @@
+{-# LANGUAGE BlockArguments #-}
+
+module Sequence
+( Sequence(..)
+, getSequenceElements
+, renderSequence
+) where
+
+import Control.Monad (replicateM)
+import Data.List
+import qualified Data.Set as S
+import System.Random
+
+import Aubio
+import Sound
+import Util
+
+data Sequence = Sequence [[Int]]
+
+data ProcessedFile = ProcessedFile Sound [Double]
+
+getSequenceElements :: Sequence -> [Int]
+getSequenceElements (Sequence measures) =
+  sort $ S.toList $ S.unions $ map S.fromList measures
+
+processFile :: String -> IO ProcessedFile
+processFile filename = do
+  sound <- readSound filename
+  track <- aubioTrack filename
+  return $ ProcessedFile sound track
+
+renderSequence :: Sequence -> [String] -> IO ()
+renderSequence sequence filenames = do
+  let numLoops = length (getSequenceElements sequence)
+  pfs <- mapM processFile filenames
+  loops <- getRandomLoops numLoops pfs
+  flip mapM (zip [0..] loops) $ \(i, loop) -> do
+    writeSound ("loop" ++ (show i) ++ ".wav") loop
+  return ()
+
+getRandomLoops :: Int -> [ProcessedFile] -> IO [Sound]
+getRandomLoops numLoops pfs = do
+  let loopSig = 4
+  setStdGen $ mkStdGen 23
+  replicateM numLoops do
+    pfNum <- getStdRandom (randomR (0, length pfs - 1))
+    msp $ "pf " ++ show pfNum
+    let ProcessedFile sound track = pfs !! pfNum
+    startTick <- getStdRandom (randomR (0, length track - loopSig))
+    let ticks = drop startTick track
+    let start = ticks !! 0
+    let end = ticks !! loopSig
+    msp $ ("snip", startTick, start, end)
+    return $ snip start end sound
