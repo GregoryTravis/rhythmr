03fba96e42def3c322ae215b050e5551b4cee3d8
Gregory Travis <greg.m.travis@gmail.com>
2019-11-17 15:33:02 -0500

    bigger sequence

--- src/Main.hs
+++ src/Main.hs
@@ -15,7 +15,16 @@ import Util
 toSequence :: [[Int]] -> Sequence Int
 toSequence nses = Seq (map (\ns -> Par (map Elem ns)) nses)
 --theSequence = Seq [Par [Elem 1], Par [Elem 1, Elem 2]]
-theSequence = toSequence [[1], [1, 2]]
+theSequence = toSequence
+  [ [8]
+  , [8, 2]
+  , [8, 2, 3]
+  , [8, 2, 3, 4]
+  , [8, 3]
+  , [2, 4]
+  , [2, 5, 6]
+  , [2, 5, 7]
+  , [1] ]
 
 main = do
   let file = "Grace Jones - Pull Up To The Bumper-Tc1IphRx1pk.f135.wav"
