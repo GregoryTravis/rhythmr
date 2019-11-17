36997d356245d743a17810e7730c7862fdcbe5dd
Gregory Travis <greg.m.travis@gmail.com>
2019-11-17 11:04:55 -0500

    samplerate field

--- g
+++ g
@@ -1,3 +1,4 @@
 #rm -f out?.wav
 #python audiolabs-erlangen.py
+rm snip.wav
 (stack build autobeat && stack exec autobeat) 2>&1 | tee out
--- src/Sound.hs
+++ src/Sound.hs
@@ -10,21 +10,23 @@ import qualified Data.StorableVector as SV
 
 import Util
 
-data Sound = Sound (SV.Vector Float)
+data Sound = Sound { samples :: SV.Vector Float
+                   , sampleRate :: Int }
 
 readSound :: String -> IO Sound
 readSound filename = do
   (info, Just (buffer :: BV.Buffer Float)) <- SF.readFile filename
   massert "sections != 1" (sections info == 1) 
   massert "channels != 2" (channels info == 2)
-  return $ Sound (BV.fromBuffer buffer)
+  return $ Sound { samples = BV.fromBuffer buffer
+                 , sampleRate = samplerate info }
 
 writeSound :: String -> Sound -> IO ()
-writeSound filename (Sound samples) = do
-  let numFrames = (SV.length samples) `div` 2
+writeSound filename sound = do
+  let numFrames = (SV.length (samples sound)) `div` 2
   let info = Info
         { frames = numFrames
-        , samplerate = 44100
+        , samplerate = sampleRate sound
         , channels = 2
         , format = Format
             { headerFormat = HeaderFormatWav
@@ -34,12 +36,14 @@ writeSound filename (Sound samples) = do
         , sections = 1
         , seekable = True
         }
-  numFramesWritten <- SF.writeFile info filename (BV.toBuffer samples)
+  numFramesWritten <- SF.writeFile info filename (BV.toBuffer (samples sound))
   massert "writeSound" (numFramesWritten == numFrames)
 
 --clop :: SV.Vector Float -> SV.Vector Float
 -- start, end: start and end (seconds)
 snip :: Double -> Double -> Sound -> Sound
-snip start end (Sound samples) = Sound $ SV.take numFrames (SV.drop startFrame samples)
-  where startFrame = esp $ 2 * (floor $ start * 44100)
-        numFrames = esp $ 2 * (floor $ (end - start) * 44100)
+snip start end sound = Sound { samples = newSamples, sampleRate = sr }
+  where startFrame = esp $ 2 * (floor $ start * (fromIntegral sr))
+        numFrames = esp $ 2 * (floor $ (end - start) * (fromIntegral sr))
+        sr = sampleRate sound
+        newSamples = SV.take numFrames (SV.drop startFrame (samples sound))
