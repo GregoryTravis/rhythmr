440ca21d238bf3a8f2b111af922102e4b38b3ba8
Gregory Travis <greg.m.travis@gmail.com>
2019-12-08 16:32:07 -0500

    remove sampleRate field

--- src/Sound.hs
+++ src/Sound.hs
@@ -11,28 +11,20 @@ import qualified Data.StorableVector as SV
 
 import Util
 
-data Sound = Sound { samples :: SV.Vector Float
-                   , sampleRate :: Int }
+data Sound = Sound { samples :: SV.Vector Float }
 
 applyToSamples :: (SV.Vector Float -> SV.Vector Float) -> Sound -> Sound
 applyToSamples f sound = sound { samples = f (samples sound) }
 
 applyToSamples2 :: (SV.Vector Float -> SV.Vector Float -> SV.Vector Float) -> Sound -> Sound -> Sound
-applyToSamples2 f a b = Sound { samples = f (samples a) (samples b)
-                              , sampleRate = sameSr }
-  where sameSr = sampleRate a -- assertM (show ("sample rates differ", map sampleRate [a, b])) (sameSampleRates [a, b]) (sampleRate a)
-
-sameSampleRates :: [Sound] -> Bool
-sameSampleRates sounds = length srs == 1
-  where srs = nub $ map sampleRate sounds
+applyToSamples2 f a b = Sound { samples = f (samples a) (samples b) }
 
 readSound :: String -> IO Sound
 readSound filename = do
   (info, Just (buffer :: BV.Buffer Float)) <- SF.readFile filename
   massert "sections != 1" (sections info == 1) 
   massert "channels != 2" (channels info == 2)
-  return $ Sound { samples = BV.fromBuffer buffer
-                 , sampleRate = samplerate info }
+  return $ Sound { samples = BV.fromBuffer buffer }
 
 numFrames :: Sound -> Int
 numFrames sound = (SV.length (samples sound)) `div` 2
@@ -42,7 +34,7 @@ writeSound filename sound = do
   let numFrames = (SV.length (samples sound)) `div` 2
   let info = Info
         { frames = numFrames
-        , samplerate = sampleRate sound
+        , samplerate = 44100
         , channels = 2
         , format = Format
             { headerFormat = HeaderFormatWav
@@ -58,10 +50,9 @@ writeSound filename sound = do
 --clop :: SV.Vector Float -> SV.Vector Float
 -- start, end: start and end (seconds)
 snip :: Int -> Int -> Sound -> Sound
-snip start end sound = Sound { samples = newSamples, sampleRate = sr }
+snip start end sound = Sound { samples = newSamples }
   where startFrame = esp $ 2 * start
         numFrames = esp $ 2 * (end - start)
-        sr = sampleRate sound
         newSamples = SV.take numFrames (SV.drop startFrame (samples sound))
 
 mixSounds :: [Sound] -> Sound
@@ -72,10 +63,8 @@ mix2Sounds :: Sound -> Sound -> Sound
 mix2Sounds a b = applyToSamples2 (SV.zipWith (+)) a b
 
 appendSounds :: [Sound] -> Sound
-appendSounds sounds = Sound { samples = newSamples
-                            , sampleRate = newSr }
+appendSounds sounds = Sound { samples = newSamples }
   where newSamples = SV.concat (map samples sounds)
-        newSr = assertM "sample rates the same" (sameSampleRates sounds) (sampleRate (head sounds))
 
 normalize :: Sound -> Sound
 normalize sound = applyToSamples (SV.map (/mx)) sound
