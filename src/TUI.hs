b8865ee432a48f4c8f251113f6a2733ffa3f5c7d
Gregory Travis <greg.m.travis@gmail.com>
2020-01-13 20:53:59 -0500

    editor and state

--- src/TUI.hs
+++ src/TUI.hs
@@ -7,6 +7,25 @@ import System.Posix.Terminal
 
 import Util
 
+---- jditor
+
+-- Bool is exit?
+type KeyboardHandler s = s -> Char -> (s, Bool)
+type Displayer s = s -> String
+
+editor :: s -> KeyboardHandler s -> Displayer s -> IO ()
+editor initState keyboardHandler displayer = do
+  let loop s = do
+        c <- getChar
+        msp $ "char " ++ (show c)
+        let (s', exitP) = keyboardHandler s c
+        msp $ displayer s'
+        if exitP then return () else loop s'
+   in loop initState
+  msp "editor done"
+
+----
+
 -- Taken from https://stackoverflow.com/questions/23068218/haskell-read-raw-keyboard-input/36297897#36297897
 withRawInput :: Int -> Int -> IO a -> IO a
 withRawInput vmin vtime action = do
@@ -34,10 +53,22 @@ withRawInput vmin vtime action = do
    -}
   action `finally` revert
 
+data State = State [Char]
+
+keyboardHandler :: KeyboardHandler State
+keyboardHandler (State cs) c = do
+  let cs' = cs ++ [c]
+      exitP = c == '\ESC'
+   in (State cs', exitP)
+
+displayer :: Displayer State
+displayer (State cs) = show cs
+
 tui = withRawInput 0 1 $ do
-  let loop = do
-        c <- getChar
-        msp $ "char " ++ (show c)
-        if c /= '\ESC' then loop else return ()
-   in loop
+  -- let loop = do
+  --       c <- getChar
+  --       msp $ "char " ++ (show c)
+  --       if c /= '\ESC' then loop else return ()
+  --  in loop
+  editor (State []) keyboardHandler displayer
   msp "tui"
