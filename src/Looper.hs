40b98d4e5134c1d370980008d3e668d5a091421a
Gregory Travis <greg.m.travis@gmail.com>
2020-01-18 12:09:10 -0500

    remove old looper

--- src/Looper.hs
+++ src/Looper.hs
@@ -1,11 +1,7 @@
 module Looper
-( Looper
-, LoopCommand(..)
-, Nooper
+( Nooper
 , withNooper
 , setSound
-, withLooper
-, sendCommand
 ) where
 
 import Control.Concurrent (forkIO, killThread)
@@ -25,7 +21,6 @@ foreign import ccall "init_audio" init_audio :: IO ()
 foreign import ccall "write_audio" write_audio :: Ptr Float -> Int -> IO ()
 foreign import ccall "term_audio" term_audio :: IO ()
 
---granularity = 11025
 granularity = 64
 
 data Nooper = Nooper (MVar Sound)
@@ -58,72 +53,6 @@ noop' l@(Nooper sv) currentIndex = do
       nextCurrentIndex = (currentIndex + (grainLength * 1)) `mod` (SV.length buffer)
   writeAudioAllAtOnce grain
   noop' l nextCurrentIndex
-
--- TODO: StopAndWait?
-data LoopCommand = Play Sound | Stop deriving Show
--- TODO probably doesn't need to be a Maybe, or even part of this?
-data Looper = Looper (Chan LoopCommand) (IORef (Maybe Sound)) -- (MV.MVar Sound)
-
-withLooper :: (Looper -> IO a) -> IO a
-withLooper f = do
-  looper <- startLooper
-  let cleanup = sendCommand looper Stop
-  (f looper) `finally` cleanup
-
-startLooper = do
-  init_audio
-  chan <- newChan
-  --cur <- MV.newEmptyMVar
-  cur <- newIORef Nothing
-  let looper = Looper chan cur
-  forkIO (backgroundLooper looper)
-  return looper
-
-backgroundLooper :: Looper -> IO ()
-backgroundLooper looper = do
-  waitForFirstSound looper
-  forkIO $ updateIORef looper
-  playCurrentSound4EVA looper
-  msp "backgroundLooper done"
-
-waitForFirstSound :: Looper -> IO ()
-waitForFirstSound (Looper chan ioref) = do
-  command <- readChan chan
-  --msp $ "Got " ++ (show command)
-  case command of Play sound -> do writeIORef ioref (Just sound)
-                                   return ()
-
-updateIORef :: Looper -> IO ()
-updateIORef looper@(Looper chan ioref) = do
-  command <- readChan chan
-  --msp $ "Got " ++ (show command)
-  handle command
-  where handle :: LoopCommand -> IO ()
-        handle (Play sound) = do
-          --msp $ "Setting " ++ (show sound)
-          writeIORef ioref (Just sound)
-          updateIORef looper
-        handle Stop = do
-          msp $ "Stopping"
-          writeIORef ioref Nothing
-          msp "updateIORef done"
-
--- Assumes ioref is not Nothing
-playCurrentSound4EVA :: Looper -> IO ()
-playCurrentSound4EVA looper@(Looper chan ioref) = do
-  soundMaybe <- readIORef ioref
-  playOrQuit soundMaybe
-  where playOrQuit :: Maybe Sound -> IO ()
-        playOrQuit (Just sound) = do
-          --msp $ "Playing " ++ (show sound)
-          let Sound { samples = buffer } = sound
-          writeAudioAllAtOnce buffer
-          playCurrentSound4EVA looper
-        playOrQuit Nothing = return ()
-
-sendCommand :: Looper -> LoopCommand -> IO ()
-sendCommand (Looper chan _) command = do
-  writeChan chan command
 
 writeAudioAllAtOnce :: Vector Float -> IO ()
 writeAudioAllAtOnce v = do
