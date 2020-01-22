18d6c56a0c1184ebd312847905179d30bf443cdd
Gregory Travis <greg.m.travis@gmail.com>
2020-01-21 19:09:03 -0500

    chorus

--- src/FX.hs
+++ src/FX.hs
@@ -6,7 +6,7 @@ import External
 import Sound
 import Util
 
-data FX = NoFX | Highpass Int | Lowpass Int
+data FX = NoFX | Highpass Int | Lowpass Int | Chorus
   deriving Show
 
 --ssRun :: (String -> String -> [String]) -> (Sound -> IO Sound)
@@ -20,3 +20,5 @@ applyFX NoFX s = return s
 
 applyFX (Highpass freq) s = ssRun ["highpass", "-2", show freq] s
 applyFX (Lowpass freq) s = ssRun ["lowpass", "-2", show freq] s
+
+applyFX Chorus s = ssRun ["chorus", "0.7", "0.9", "55", "0.4", "0.25", "2", "-t", "60", "0.32", "0.4", "2.3", "-t", "40", "0.3", "0.3", "1.3", "-s"] s
