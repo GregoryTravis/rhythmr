3aaf55fc499523bd41e41ff855a46bc9483bc6e1
Gregory Travis <greg.m.travis@gmail.com>
2020-05-27 16:14:27 -0400

    Don't copy tracks out of memo, just write loops directly to collection. No more loops/ dir. All commands take a collection param

--- src/Bars.hs
+++ src/Bars.hs
@@ -1,6 +1,6 @@
 module Bars
 ( barsSearch
-, barsYouTubeURL
+, barsId
 , barsFile
 ) where
 
@@ -21,22 +21,21 @@ doSpleeter = False
 
 searchAndDownloadFiles :: String -> String -> Int -> IO [FilePath]
 searchAndDownloadFiles collection searchString count = do
-  createDirectoryIfMissing False collection
   ids <- search searchString count
-  msp "ids"
-  msp ids
+  msp ("ids", ids)
   filenames <- mapM download ids
-  mapM save filenames
-  where save filename = do
-          msp ("copy", filename, dest filename)
-          copyFile filename (dest filename)
-          return $ dest filename
-        dest filename = collection ++ "/" ++ (takeBaseName filename) ++ ".wav"
+  return filenames
+  -- mapM save filenames
+  -- where save filename = do
+  --         msp ("copy", filename, dest filename)
+  --         copyFile filename (dest filename)
+  --         return $ dest filename
+  --       dest filename = collection ++ "/" ++ (takeBaseName filename) ++ ".wav"
 
 barsSearch :: String -> String -> Int -> IO ()
 barsSearch collection searchString numTracks = do
   filenames <- searchAndDownloadFiles collection searchString 8
-  mapM_ extractLoops filenames
+  mapM_ (extractLoops collection) filenames
 
 youtubeUrlPrefix = "https://www.youtube.com/watch?v="
 
@@ -45,22 +44,23 @@ toId s = case stripPrefix youtubeUrlPrefix s of Just s -> s
                                                 Nothing -> s
 
 -- takes a url or id
-barsYouTubeURL :: String -> IO ()
-barsYouTubeURL idOrUrl = do
+barsId :: String -> String -> IO ()
+barsId collection idOrUrl = do
   --let destFilename = "tracks/" ++ id
   filename <- download (toId idOrUrl)
   --renameFile filename destFilename
   --msp destFilename
-  extractLoops filename
+  extractLoops collection filename
 
-barsFile :: String -> IO ()
-barsFile filename = do
+barsFile :: String -> String -> IO ()
+barsFile collection filename = do
   isDir <- doesDirectoryExist filename
   files <- if isDir then getDirRecursive filename
                     else return [filename]
-  mapM_ extractLoops files
+  mapM_ (extractLoops collection) files
 
-extractLoops filename = do
+extractLoops collection filename = do
+  createDirectoryIfMissing False collection
   msp filename
   bars <- fmap (take 40 . drop 10) $ barBeat filename
   original <- readZound filename
@@ -76,7 +76,7 @@ extractLoops filename = do
   where writer :: Zound -> String -> IO ()
         writer sound filename = writeZound filename sound
         writeZounds :: [Zound] -> IO [String]
-        writeZounds sounds = mapM (contentAddressableWrite fileStub "loops" "wav" . writer) sounds
+        writeZounds sounds = mapM (contentAddressableWrite fileStub collection "wav" . writer) sounds
         fileStub = "loop-" ++ takeBaseName filename
 
 splitIntoLoops :: Zound -> [Int] -> [Zound]
