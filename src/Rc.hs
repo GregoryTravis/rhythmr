b357d4733da2f332cc7adafa4893845516004bc3
Gregory Travis <greg.m.travis@gmail.com>
2020-07-28 17:27:35 -0400

    handle missing .rhythmrc

--- src/Rc.hs
+++ src/Rc.hs
@@ -6,16 +6,24 @@ import Data.Aeson
 import Data.ByteString.Lazy.UTF8 as BLU (fromString)
 import Data.Maybe
 import GHC.Generics
+import System.Directory (doesFileExist)
 import System.IO
 import System.IO.Unsafe (unsafePerformIO)
 
+rcFile = ".rhythmrc"
+
 data Rc = Rc { cacheDownloads :: Bool } deriving (Generic, Show)
+
+defaultRc :: Rc
+defaultRc = Rc { cacheDownloads = False }
 
 instance FromJSON Rc
 
 readRc :: IO Rc
 readRc = do
-  rcString <- BLU.fromString <$> readFile ".rhythmrc"
-  return $ fromJust $ decode rcString
+  b <- doesFileExist rcFile
+  if b then do rcString <- BLU.fromString <$> readFile rcFile
+               return $ fromJust $ decode rcString
+       else return defaultRc
 
 rc = unsafePerformIO readRc
