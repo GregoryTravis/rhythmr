0f8d99a78d3a14d61048e48c07fd8d98907f31df
Gregory Travis <greg.m.travis@gmail.com>
2020-10-02 17:12:25 -0400

    stow a W

--- src/Stow.hs
+++ src/Stow.hs
@@ -4,19 +4,57 @@
 module Stow
 ( stowMain ) where
 
+import System.Directory (createDirectoryIfMissing)
+import System.IO.Unsafe
+
+import Hash
 import Util
 
-class Stowable a where
-  stow :: a -> String
+stowDir :: String
+stowDir = ".stow"
 
-instance Show a => Stowable a where
+type Key = String
+
+class Show a => Stowable a where
+  stow :: a -> Key
   stow = show
+
+-- Maybe this should be the reverse?
+-- instance Show a => Stowable a where
+--   stow = show
+
+data StoredIndirect = StoredIndirect Key
+  deriving Show
+
+data Indirect a = Indirect Key (Maybe a)
+instance Show (Indirect a) where
+  show (Indirect key _) = show (StoredIndirect key)
+
+indirect :: (Stowable a) => a -> Indirect a
+indirect x =
+  let s = stow x
+      key = hash s
+      filename = makeFilename key
+      writeIt = writeFile filename s
+   in (unsafePerformIO writeIt) `seq` Indirect key (Just x)
+
+makeFilename :: Key -> FilePath
+makeFilename key = (unsafePerformIO ensureDir) `seq` (stowDir ++ "/" ++ key)
+
+ensureDir :: IO ()
+ensureDir = createDirectoryIfMissing True stowDir
+
+saveRoot :: Stowable a => a -> Indirect a
+saveRoot = indirect
 
 data W = W { ints :: [Int]
            , string :: String }
   deriving Show
 
+instance Stowable W
+
 stowMain = do
   let w = W { ints = [1, 2], string = "asdf" }
-  msp $ stow w
+  msp $ saveRoot w
+  --msp $ stow w
   msp "hi"
