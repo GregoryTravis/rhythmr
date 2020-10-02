75862c8f096688e59c18f56a3bc9e0038d2ff426
Gregory Travis <greg.m.travis@gmail.com>
2020-10-02 16:27:21 -0400

    stowable

--- src/Stow.hs
+++ src/Stow.hs
@@ -1,7 +1,22 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module Stow
 ( stowMain ) where
 
 import Util
 
+class Stowable a where
+  stow :: a -> String
+
+instance Show a => Stowable a where
+  stow = show
+
+data W = W { ints :: [Int]
+           , string :: String }
+  deriving Show
+
 stowMain = do
+  let w = W { ints = [1, 2], string = "asdf" }
+  msp $ stow w
   msp "hi"
