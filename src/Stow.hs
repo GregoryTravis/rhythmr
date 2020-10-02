210d90aed1aac9926f8d8b6bd14d19889e4c9c1e
Gregory Travis <greg.m.travis@gmail.com>
2020-10-02 17:47:47 -0400

    still love the ol persistencing

--- src/Stow.hs
+++ src/Stow.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE UndecidableInstances #-}
 
+-- error if the file exists
+
 module Stow
 ( stowMain ) where
 
@@ -23,12 +25,18 @@ class Show a => Stowable a where
 -- instance Show a => Stowable a where
 --   stow = show
 
-data StoredIndirect = StoredIndirect Key
-  deriving Show
+data StoredIndirect a = StoredIndirect Key
+  deriving (Read, Show)
 
 data Indirect a = Indirect Key (Maybe a)
 instance Show (Indirect a) where
   show (Indirect key _) = show (StoredIndirect key)
+instance Read (Indirect a) where
+  readsPrec prec s = map unSI $ (readsPrec prec s :: [(StoredIndirect a, String)])
+    where unSI (StoredIndirect key, rest) = (Indirect key Nothing, rest)
+
+keyOf :: Indirect a -> Key
+keyOf (Indirect key _) = key
 
 indirect :: (Stowable a) => a -> Indirect a
 indirect x =
@@ -44,17 +52,31 @@ makeFilename key = (unsafePerformIO ensureDir) `seq` (stowDir ++ "/" ++ key)
 ensureDir :: IO ()
 ensureDir = createDirectoryIfMissing True stowDir
 
-saveRoot :: Stowable a => a -> Indirect a
-saveRoot = indirect
+saveRoot :: Stowable a => a -> Key
+saveRoot = keyOf . indirect
+
+loadRoot :: Read a => Key -> a
+loadRoot key = case retrieve (Indirect key Nothing) of Indirect _ (Just x) -> x
+
+retrieve :: Read a => Indirect a -> Indirect a
+--retrieve (Indirect key (Just x)) = x
+retrieve (Indirect key Nothing) = x `seq` (Indirect key (Just x))
+  where x = unsafePerformIO $ do
+          s <- ((readFile (makeFilename key)) :: IO String)
+          return $ read s
+retrieve ind = ind
 
 data W = W { ints :: [Int]
            , string :: String }
-  deriving Show
+  deriving (Read, Show)
 
 instance Stowable W
 
 stowMain = do
   let w = W { ints = [1, 2], string = "asdf" }
-  msp $ saveRoot w
-  --msp $ stow w
+  let saved = saveRoot w
+  msp saved
+  let w' :: W
+      w' = loadRoot saved
+  msp w'
   msp "hi"
