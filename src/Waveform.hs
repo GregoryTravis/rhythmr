ba7c58ac9f13e0f60854f30584608d8bdf3acf2f
Gregory Travis <greg.m.travis@gmail.com>
2021-01-09 13:39:26 -0500

    using bytestring and looks wrong and also no faster because I did not use builder

--- src/Waveform.hs
+++ src/Waveform.hs
@@ -3,8 +3,11 @@ module Waveform
 , loopToWaveformUnsafe
 ) where
 
-import Data.ByteString (ByteString, pack)
-import Data.List (transpose)
+import Prelude hiding (concat, replicate)
+
+import qualified Data.ByteString.Lazy as L
+import Data.ByteString.Builder
+import Data.Int (Int64)
 import Data.Word
 import Graphics.Gloss
 import Graphics.Gloss.Data.Color
@@ -16,10 +19,16 @@ import Loop
 import Util
 import Zounds
 
-fg :: [Word8]
-fg = [40, 40, 200, 200]
-bg :: [Word8]
-bg = [128, 0, 128, 64]
+-- Efficiently generate a waveform image of a Zound. This generates it rotated
+-- 90 degrees since it's simpler and un-rotating it during GPU rendering is
+-- cheap (free?)
+
+type Col = L.ByteString
+
+fg :: Col
+fg = L.pack [40, 40, 200, 200]
+bg :: Col
+bg = L.pack [128, 0, 128, 64]
 
 -- I am deeply ashamed; but all the files are already there and don't move or
 -- change, so it should work.
@@ -34,25 +43,39 @@ loopToWaveform projectDir w h (Loop filename) = do
   return $ zoundToWaveform w h (normalize z)
 
 zoundToWaveform :: Int -> Int -> Zound -> Picture
-zoundToWaveform w h z = bitmapOfByteString w h (BitmapFormat TopToBottom PxRGBA) packed True
-  where packed = pack $ generatePixels w h z
+zoundToWaveform w h z = bitmapOfByteString w h (BitmapFormat TopToBottom PxRGBA) bs True
+  where bs = L.toStrict $ generatePixels w h z
 
-generatePixels :: Int -> Int -> Zound -> [Word8]
-generatePixels w h z = concat $ concat $ {-eeesp "columns" $-} transpose columns
-  where columns = map generateCol rmses
+generatePixels :: Int -> Int -> Zound -> L.ByteString
+generatePixels w h z = mconcat $ L.transpose rows
+  where rows :: [L.ByteString]
+        rows = map (generateRow fg bg (fromIntegral w)) waveWidths
+        rmses :: [Float]
         rmses = rmsTo w z
-        generateCol :: Float -> [[Word8]]
-        generateCol s = {-eeesp ("lens", length top, length middle, length bottom) $-} top ++ middle ++ bottom
-          where middle = nOf ht fg
-                top = nOf ((h - ht) `div` 2) bg
-                bottom = nOf (h - ht - length top) bg
-                --ht = eeesp ("ht", s) $ clip 0 (h-1) $ floor $ s * fromIntegral (h `div` 2)
-                --ht = eeesp ("ht", s) $ clip 0 (h-1) $ floor (((s * fromIntegral h) / 2) + fromIntegral (h `div` 2))
-                -- [0,1] -> [0,h/2]
-                ht = clip 2 (h-1) $ floor $ s * ((fromIntegral h) / 2)
+        waveWidths :: [Int64]
+        waveWidths = [fromIntegral $ clip 2 (h-1) $ floor $ s * ((fromIntegral h) / 2) | s <- rmses]
+        --generateCol :: Float -> L.ByteString
+        --generateCol s = {-eeesp ("lens", length top, length middle, length bottom) $-} top `mconcat` middle `mconcat` bottom
+        --  where middle = L.replicate ht fg
+        --        top = L.replicate (fromIntegral ((h - ht) `div` 2)) bg
+        --        bottom = L.replicate (h - ht - length top) bg
+        --        --ht = eeesp ("ht", s) $ clip 0 (h-1) $ floor $ s * fromIntegral (h `div` 2)
+        --        --ht = eeesp ("ht", s) $ clip 0 (h-1) $ floor (((s * fromIntegral h) / 2) + fromIntegral (h `div` 2))
+        --        -- [0,1] -> [0,h/2]
+        --        ht = clip 2 (h-1) $ floor $ s * ((fromIntegral h) / 2)
 
-nOf :: Int -> a -> [a]
-nOf n x = take n (repeat x)
+-- Generate a row (actually a column of a waveform) like:
+--     bbbbbbbbbbffffffffffffffffffffffbbbbbbbbbb
+generateRow :: Col -> Col -> Int64 -> Int64 -> L.ByteString
+generateRow fg bg totalLen middleLen = left `mappend` middle `mappend` right
+  where left = L.take (leftLen * 4) (L.cycle bg)
+        middle = L.take (middleLen * 4) (L.cycle fg)
+        right = L.take (rightLen * 4) (L.cycle bg)
+        leftLen = (totalLen - middleLen) `div` 2
+        rightLen = totalLen - middleLen - leftLen
+
+-- nOf :: Int -> a -> [a]
+-- nOf n x = take n (repeat x)
 
 clip :: (Num a, Ord a) => a -> a -> a -> a
 clip low high x
